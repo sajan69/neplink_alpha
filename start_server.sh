c9d93bc02db4005e390d37320aeab881f1c6c15b
Sajan Adhikari <120996556+sajan69@users.noreply.github.com>
2025-03-13 20:50:10 +0545

    fix: update start_server.sh to use /bin/sh and correct base64 decode command

--- start_server.sh
+++ start_server.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
 # Decode Firebase credentials from environment variable and save to file
-echo "$FIREBASE_CREDENTIALS" | base64 --decode > ./firebase_credentials.json
+echo "$FIREBASE_CREDENTIALS" | base64 -d > ./firebase_credentials.json
 
 # Start Gunicorn server
-exec gunicorn neplink.wsgi:application --bind 0.0.0.0:8080 
+gunicorn neplink.wsgi:application --bind 0.0.0.0:8080 
